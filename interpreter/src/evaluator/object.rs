a21ca571f903358c61dc2f5e0fde2a12088f9121
ultraman <1394466835@qq.com>
2024-10-09 20:52:12 +0800

    update

---
+++ interpreter/src/evaluator/object.rs
@@ -0,0 +1,23 @@
+use core::fmt;
+
+pub enum Object {
+    Integer(i64),
+    Boolean(bool),
+    Null,
+}
+
+impl fmt::Display for Object {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        match *self {
+            Object::Integer(ref v) => write!(f, "{}", v),
+            Object::Boolean(ref v) => {
+                if *v {
+                    write!(f, "true")
+                } else {
+                    write!(f, "false")
+                }
+            }
+            Object::Null => write!(f, "null"),
+        }
+    }
+}
